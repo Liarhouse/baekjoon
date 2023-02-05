cf2d159d057140e4b2e7748ecf041ff41c34d44c
Donghwan Jung <116786879+Liarhouse@users.noreply.github.com>
2023-02-05 23:41:08 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1136 ms, Memory: 40352 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,26 @@
+def sosu(n):
+    if n ==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True							
+
+all_list = list(range(2,246912))		
+memo = []								
+
+for i in all_list:						
+    if sosu(i):							
+        memo.append(i)					
+
+n = int(input())
+
+while True:
+    count=0					
+    if n == 0 :
+            break
+    for i in memo:			
+        if n < i <=2*n:		
+            count+=1		
+    print(count)
+    n = int(input())
