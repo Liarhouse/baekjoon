f07a688deda3c4a258f947bf03346c377bf9b3e1
Donghwan Jung <116786879+Liarhouse@users.noreply.github.com>
2023-05-02 10:02:35 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,12 @@
+x=int(input())
+y=int(input())
+z=int(input())
+if x + y + z != 180:
+    print('Error')
+else:
+    if x == 60 and y==60 and z==60:
+        print('Equilateral')
+    elif x==y or x==z or y==z:
+        print('Isosceles')
+    else:
+        print('Scalene')
